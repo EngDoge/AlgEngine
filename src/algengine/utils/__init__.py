ea45a09d3e37e7cfb7066cc879fa37687f8a6f6a
bill <billsun930@qq.com>
2026-02-10 16:31:25 +0800

    init commit

--- src/algengine/utils/__init__.py
+++ src/algengine/utils/__init__.py
@@ -2,6 +2,7 @@ from .archive import ArchiveManager
 from .config import Config
 from .device import parse_device_id, parse_cuda_device_id
 from .formatter import SuffixFormatter, PathFormatter
+from .io import load_from
 from .misc import exists_or_make, is_none, is_not_none, convert2map, get_local_ip, is_local_port_occupied
 from .recorder import ActionRecorder
 from .registry import Registry
@@ -12,6 +13,7 @@ __all__ = [
     'Config',
     'parse_device_id', 'parse_cuda_device_id',
     'SuffixFormatter', 'PathFormatter',
+    'load_from',
     'ActionRecorder',
     'exists_or_make', 'is_none', 'is_not_none', 'convert2map', 'get_local_ip', 'is_local_port_occupied',
     'Registry',
